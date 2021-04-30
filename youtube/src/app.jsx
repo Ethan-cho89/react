9739200a2756819ec97f579d7f38735d12e3c57f
ethan <bjaguar89@gmail.com>
2021-04-30 17:40:41 +0900

    first step

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyB_RCpW6_bz2EekkJjJLTz_uWzSQw4ASKU&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB_RCpW6_bz2EekkJjJLTz_uWzSQw4ASKU',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
   return (
-    <h1>hello</h1>
-  );  
+    <>
+      <VideoList videos={videos} />
+    </>
+  );
 }
 
 export default App;
