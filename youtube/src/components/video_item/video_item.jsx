38641fb6fa4ee2191153b4ba5b8c608b8287156c
Donghyun_cho <bjaguar89@gmail.com>
2021-05-01 13:10:26 +0900

    make video item

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import styles from "./video_item.module.css";
 
-const Video = (props) => {
-  return <li>{props.video.snippet.title}</li>;
+const VideoItem = ({ video: { snippet } }) => {
+  //deconstruction으로 props안에 video가 있으면 바로 쓸수 있다
+  // ({video : 새이름}) 이렇게 하면 video에 대한 변수명을 바꿀수 있다
+  // ({ video: { snippet } }) 이렇게 하면 props내의 video안에 snippet을 잡는다
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="videoThumbnail"
+        ></img>
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
 };
 
-export default Video;
+export default VideoItem;
