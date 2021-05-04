b5efacb5911bdf83cbd521676f09ea6b12ffe058
ethan <bjaguar89@gmail.com>
2021-05-04 12:23:39 +0900

    almost done

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({ video: { snippet } }) => {
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
   //deconstruction으로 props안에 video가 있으면 바로 쓸수 있다
   // ({video : 새이름}) 이렇게 하면 video에 대한 변수명을 바꿀수 있다
   // ({ video: { snippet } }) 이렇게 하면 props내의 video안에 snippet을 잡는다
+  const displayType = display === 'list' ? styles.list : styles.grid;
   return (
-    <li className={styles.container}>
+    <li
+      className={`{styles.container} ${displayType}`}
+      onClick={() => {
+        onVideoClick(video);
+      }}
+    >
       <div className={styles.video}>
         <img
           className={styles.thumbnail}
--- youtube/src/service/youtubeService.js
+++ youtube/src/service/youtubeService.js
@@ -10,18 +10,16 @@ class YoutubeServer{
   
   async mostPopular() {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=5&key=${this.key}`,
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=${this.key}`,
       this.getRequestOptions
     );
-    console.log("////// response /////");
-    console.log(response);
     const result = await response.json();
     return result.items;
   } //만들어진 프로미스 리턴
 
   async search(query) {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${query}&type=video&key=${this.key}`,
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
     );
     const result = await response.json();
